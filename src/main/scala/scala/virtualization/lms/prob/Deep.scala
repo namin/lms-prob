84b9a33dc3ec8846ff39feb2570231cfd67fd64a
amin <nada.amin@epfl.ch>
2013-02-13 16:15:08 +0100

    etoffer

--- src/main/scala/scala/virtualization/lms/prob/Deep.scala
+++ src/main/scala/scala/virtualization/lms/prob/Deep.scala
@@ -30,7 +30,17 @@ trait ProbPrettyPrint extends ProbIntf {
   }
 }
 
-trait ProbCore extends ProbIntf with ProbPrettyPrint {
+trait ProbLang extends EmbeddedControls with ProbIntf {
+
+  def liftOp2[A,B,C](x: Rand[A], y: Rand[B])(f: (A,B) => C): Rand[C] = for (a <- x; b <- y) yield f(a,b)
+
+  def infix_&&(x: Rand[Boolean], y: Rand[Boolean]): Rand[Boolean] = liftOp2(x,y)(_ && _) // short circuit ??
+  def infix_===[A](x: Rand[A], y: Rand[A]): Rand[Boolean] =         liftOp2(x,y)(_ == _)
+  def infix_+(x: Rand[Int], y: Rand[Int]): Rand[Int] =              liftOp2(x,y)(_ + _)
+
+}
+
+trait ProbCore extends ProbIntf with ProbPrettyPrint with ProbLang {
   type Rand[+A] = RandVar[A]
   case class Choice[+A](rv: Int, v: A, p: Prob)
   type Path = List[Choice[Any]]
@@ -76,13 +86,17 @@ trait ProbCore extends ProbIntf with ProbPrettyPrint {
   }
 }
 
-trait DeepBase extends Base {
+trait DeepBase extends Base with EmbeddedControls {
   type Rand[+A]
   type Prob = Double
 
   def flip(p: Prob = 0.5): Rep[Rand[Boolean]]
   def always[A:Manifest](e: Rep[A]): Rep[Rand[A]]
   def pp[A:Manifest](e: Rep[Rand[A]]): Rep[String]
+  def __ifThenElse[T:Manifest](cond: Rep[Rand[Boolean]], thenp: => Rep[Rand[T]], elsep: => Rep[Rand[T]]): Rep[Rand[T]]
+  def infix_&&(x: Rep[Rand[Boolean]], y: Rep[Rand[Boolean]]): Rep[Rand[Boolean]]
+  def infix_===[A](x: Rep[Rand[A]], y: Rep[Rand[A]]): Rep[Rand[Boolean]]
+  def infix_+(x: Rep[Rand[Int]], y: Rep[Rand[Int]]): Rep[Rand[Int]]
 }
 
 trait DeepBaseExp extends DeepBase with EffectExp {
@@ -91,10 +105,23 @@ trait DeepBaseExp extends DeepBase with EffectExp {
   case class Flip(p: Prob) extends Def[Rand[Boolean]]
   case class Always[A:Manifest](e: Exp[A]) extends Def[Rand[A]]
   case class PP[A:Manifest](e: Exp[Rand[A]]) extends Def[String]
+  case class RandIfThenElse[T:Manifest](cond: Exp[Rand[Boolean]], thenp: Block[Rand[T]], elsep: Block[Rand[T]]) extends Def[Rand[T]]
+  case class RandAnd(x: Rep[Rand[Boolean]], y: Rep[Rand[Boolean]]) extends Def[Rand[Boolean]]
+  case class RandEq[A](x: Rep[Rand[A]], y: Rep[Rand[A]]) extends Def[Rand[Boolean]]
+  case class RandPlus(x: Rep[Rand[Int]], y: Rep[Rand[Int]]) extends Def[Rand[Int]]
 
   override def flip(p: Prob) = reflectEffect(Flip(p))
   override def always[A:Manifest](e: Exp[A]) = Always(e)
   override def pp[A:Manifest](e: Exp[Rand[A]]) = PP(e)
+  override def __ifThenElse[T:Manifest](cond: Rep[Rand[Boolean]], thenp: => Rep[Rand[T]], elsep: => Rep[Rand[T]]): Rep[Rand[T]] = {
+    val a = reifyEffectsHere(thenp)
+    val b = reifyEffectsHere(elsep)
+
+    RandIfThenElse(cond,a,b)
+  }
+  override def infix_&&(x: Rep[Rand[Boolean]], y: Rep[Rand[Boolean]]): Rep[Rand[Boolean]] = RandAnd(x, y)
+  override def infix_===[A](x: Rep[Rand[A]], y: Rep[Rand[A]]): Rep[Rand[Boolean]] = RandEq(x, y)
+  override def infix_+(x: Rep[Rand[Int]], y: Rep[Rand[Int]]): Rep[Rand[Int]] = RandPlus(x, y)
 }
 
 trait ScalaGenDeepBase extends ScalaGenEffect {
@@ -105,13 +132,25 @@ trait ScalaGenDeepBase extends ScalaGenEffect {
     case Flip(p) => emitValDef(sym, "(flip(" + p + "))")
     case Always(e) => emitValDef(sym, "(always(" + quote(e) + "))")
     case PP(e) => emitValDef(sym, "(pp(" + quote(e) + "))")
+    case RandIfThenElse(c,a,b) =>
+      stream.println("val " + quote(sym) + " = " + quote(c) + ".flatMap{ c => c match {")
+      stream.println("case true =>")
+      emitBlock(a)
+      stream.println(quote(getBlockResult(a)))
+      stream.println("case false =>")
+      emitBlock(b)
+      stream.println(quote(getBlockResult(b)))
+      stream.println("}}")
+    case RandAnd(x, y) => emitValDef(sym, quote(x) + " && " + quote(y))
+    case RandEq(x, y) => emitValDef(sym, quote(x) + " === " + quote(y))
+    case RandPlus(x, y) => emitValDef(sym, quote(x) + " + " + quote(y))
     case _ => super.emitNode(sym, rhs)
   }
 }
 
-trait DeepLangBase extends DeepBase with IfThenElse with NumericOps with BooleanOps with Equal with ListOps with TupleOps with LiftNumeric with LiftBoolean
-trait DeepLangExp extends DeepLangBase with DeepBaseExp with IfThenElseExp with NumericOpsExp with BooleanOpsExp with EqualExp with ListOpsExp with TupleOpsExp
-trait ScalaGenDeepLang extends ScalaGenDeepBase with ScalaGenIfThenElse with ScalaGenNumericOps with ScalaGenBooleanOps with ScalaGenEqual with ScalaGenListOps with ScalaGenTupleOps {
+trait DeepLangBase extends DeepBase/* with IfThenElse with NumericOps with BooleanOps with Equal with ListOps with TupleOps with LiftNumeric with LiftBoolean*/
+trait DeepLangExp extends DeepLangBase with DeepBaseExp/* with IfThenElseExp with NumericOpsExp with BooleanOpsExp with EqualExp with ListOpsExp with TupleOpsExp*/
+trait ScalaGenDeepLang extends ScalaGenDeepBase/* with ScalaGenIfThenElse with ScalaGenNumericOps with ScalaGenBooleanOps with ScalaGenEqual with ScalaGenListOps with ScalaGenTupleOps*/ {
   val IR: DeepLangExp
 }
 trait DeepLang extends DeepLangExp with CompileScala { q =>
@@ -129,7 +168,27 @@ trait DeepLang extends DeepLangExp with CompileScala { q =>
 }
 
 object TestDeep extends App with DeepLang {
-  val f = (_: Rep[Unit]) => pp(flip())
-  val fc = compile(f)
-  println(fc())
+  val f1 = (_: Rep[Unit]) => pp(flip())
+  val fc1 = compile(f1)
+  println(fc1())
+
+  val f2 = (_: Rep[Unit]) => pp(if (flip()) always(unit(1)) else always(unit(0)))
+  val fc2 = compile(f2)
+  println(fc2())
+
+  val f3 = (_: Rep[Unit]) => {
+    val a = if (flip()) always(unit(1)) else always(unit(0))
+    val b = if (flip()) always(unit(1)) else always(unit(0))
+    pp(a === b)
+  }
+  val fc3 = compile(f3)
+  println(fc3())
+
+  val f4 = (_: Rep[Unit]) => {
+    val a = if (flip()) always(unit(1)) else always(unit(0))
+    val b = a
+    pp(a === b)
+  }
+  val fc4 = compile(f4)
+  println(fc4())
 }
